041b428911a5a5aa413b70a8c94eb8fd050db6e7
Phillip Ssempeebwa <phillip@debian-BULLSEYE-live-builder-AMD64>
2024-02-08 19:49:59 +0300

    First file upload

---
+++ register.php
@@ -0,0 +1,189 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+  <meta charset="utf-8">
+  <meta content="width=device-width, initial-scale=1.0" name="viewport">
+
+  <title>Proffessional Driving</title>
+  <meta content="" name="description">
+  <meta content="" name="keywords">
+
+  <!-- Favicons -->
+  <link href="assets/img/favicon.png" rel="icon">
+  <link href="assets/img/apple-touch-icon.png" rel="apple-touch-icon">
+
+  <!-- Google Fonts -->
+  <link href="https://fonts.googleapis.com/css?family=Open+Sans:300,300i,400,400i,600,600i,700,700i|Jost:300,300i,400,400i,500,500i,600,600i,700,700i|Poppins:300,300i,400,400i,500,500i,600,600i,700,700i" rel="stylesheet">
+
+  <!-- Vendor CSS Files -->
+  <link href="assets/vendor/aos/aos.css" rel="stylesheet">
+  <link href="assets/vendor/bootstrap/css/bootstrap.min.css" rel="stylesheet">
+  <link href="assets/vendor/bootstrap-icons/bootstrap-icons.css" rel="stylesheet">
+  <link href="assets/vendor/boxicons/css/boxicons.min.css" rel="stylesheet">
+  <link href="assets/vendor/glightbox/css/glightbox.min.css" rel="stylesheet">
+  <link href="assets/vendor/remixicon/remixicon.css" rel="stylesheet">
+  <!-- <link href="assets/vendor/swiper/swiper-bundle.min.css" rel="stylesheet"> -->
+
+  <!-- Template Main CSS File -->
+  <link href="assets/css/style.css" rel="stylesheet">
+
+  <!-- =======================================================
+  * Template Name: Arsha
+  * Updated: Aug 30 2023 with Bootstrap v5.3.1
+  * Template URL: https://bootstrapmade.com/arsha-free-bootstrap-html-template-corporate/
+  * Author: BootstrapMade.com
+  * License: https://bootstrapmade.com/license/
+  ======================================================== -->
+</head>
+
+<body>
+
+  <!-- ======= Header ======= -->
+  <header id="header" class="fixed-top ">
+    <div class="container d-flex align-items-center">
+
+      <h1 class="logo me-auto"><a href="index.html">Proffesional Driver</a></h1>
+      <!-- Uncomment below if you prefer to use an image logo -->
+      <!-- <a href="index.html" class="logo me-auto"><img src="assets/img/logo.png" alt="" class="img-fluid"></a>-->
+
+      <nav id="navbar" class="navbar">
+        <ul>
+          <li><a class="nav-link scrollto active" href="#hero">Home</a></li>
+          <li><a class="nav-link scrollto" href="#about">About</a></li>
+          <li><a class="nav-link scrollto" href="#services">Services</a></li>
+          <li><a class="nav-link scrollto" href="#team">Team</a></li>
+          
+          <li><a class="nav-link scrollto" href="#contact">Contact</a></li>
+          <li><a class="getstarted scrollto" href="login.php">Login</a></li>
+        </ul>
+        <i class="bi bi-list mobile-nav-toggle"></i>
+      </nav><!-- .navbar -->
+
+    </div>
+  </header><!-- End Header -->
+
+
+  <main id="main">
+<br><br><br><br><br>
+    <!-- ======= Contact Section ======= -->
+    <section id="contact" class="contact">
+      <div class="container" data-aos="fade-up">
+
+        <div class="section-title">
+          <h2>REGISTER</h2>
+          <p>We're here to ensure professional driving on the road</p>
+        </div>
+
+        <div style="display: grid; grid-template-columns: 60% 40%" class="row">
+
+          <div >
+            <form action="forms/contact.php" method="post" role="form" class="php-email-form">
+              <div class="row">
+                <div class="form-group col-md-6">
+                  <label for="name">Your Name</label>
+                  <input type="text" name="name" class="form-control" id="name" required>
+                </div>
+                <div class="form-group col-md-6">
+                  <label for="name">Your Email</label>
+                  <input type="email" class="form-control" name="email" id="email" required>
+                </div>
+              </div>
+              <div class="row">
+                <div class="form-group col-md-6">
+                  <label for="name">Gender</label>
+                  <select name="name" class="form-control" id="gender" required>
+                  <option value="">Select Gender</option>
+                    <option value="">Male</option>
+                    <option value="">Female</option>
+                    <option value="">Rather not say</option>
+                  </select>
+                </div>
+                <div class="form-group col-md-6">
+                  <label for="name">Your Age</label>
+                  <input type="number" class="form-control" name="age" id="email" required>
+                </div>
+              </div>
+              <div class="row">
+                <div class="form-group col-md-6">
+                  <label for="name">Ethnic Group</label>
+                  <select name="ethnicgp" class="form-control" id="ethnicgp" required>
+                  <option value="">Select Ethniic Group</option>
+                    <option value="">Bakiga</option>
+                    <option value="">Bagisu</option>
+                    <option value="">RBaganda</option>
+                    <option value="">Other</option>
+                  </select>
+                </div>
+                <div class="form-group col-md-6">
+                  <label for="name">Marital Status</label>
+                  <select name="ethnicgp" class="form-control" id="ethnicgp" required>
+                  <option value="">Select Marital Status</option>
+                    <option value="">Married</option>
+                    <option value="">Single</option>
+                    <option value="">Divorced</option>
+                    <option value="">Rather Not Say</option>
+                  </select>
+                </div>
+              </div>
+              <div class="row">
+                <div class="form-group ">
+                  <label for="name">Highest Level Of Education</label>
+                  <select name="ethnicgp" class="form-control" id="ethnicgp" required>
+                  <option value="">Select your Highest Level Of Education</option>
+                    <option value="">Primary School</option>
+                    <option value="">Secondary School</option>
+                    <option value="">Some College/AA Degree/Technical School Driving</option>
+                    <option value="">Graduate School Degree:Masters Or Doctorate</option>
+                  </select>
+                </div>
+                <div class="text-center"><input class="btn btn-primary" type="submit" name="submit" value="Register"/></div>
+              </div>
+              
+            </form>
+          </div>
+          <div   >
+            <img style="width:100%; height: 100%" src="./assets/img/banner-img (2).jpg" alt="SIGN UP">
+          </div>
+
+        </div>
+
+        
+
+      </div>
+    </section><!-- End Contact Section -->
+
+  </main><!-- End #main -->
+
+  <!-- ======= Footer ======= -->
+  <footer id="footer">
+
+
+    
+
+    <div class="container footer-bottom clearfix">
+      <div class="copyright">
+        &copy; Copyright <strong><span>Proffesional Driver</span></strong>. All Rights Reserved
+      </div>
+      
+    </div>
+  </footer><!-- End Footer -->
+
+  <div id="preloader"></div>
+  <a href="#" class="back-to-top d-flex align-items-center justify-content-center"><i class="bi bi-arrow-up-short"></i></a>
+
+  <!-- Vendor JS Files -->
+  <script src="assets/vendor/aos/aos.js"></script>
+  <script src="assets/vendor/bootstrap/js/bootstrap.bundle.min.js"></script>
+  <script src="assets/vendor/glightbox/js/glightbox.min.js"></script>
+  <script src="assets/vendor/isotope-layout/isotope.pkgd.min.js"></script>
+  <script src="assets/vendor/swiper/swiper-bundle.min.js"></script>
+  <script src="assets/vendor/waypoints/noframework.waypoints.js"></script>
+  <script src="assets/vendor/php-email-form/validate.js"></script>
+
+  <!-- Template Main JS File -->
+  <script src="assets/js/main.js"></script>
+
+</body>
+
+</html>
